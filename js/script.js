db1f1c3572f9ce2b5007c7ae5d7e55685c9e1e4c
Maciej Olejniczak <olejniczakk.maciejj@gmail.com>
2020-03-07 16:33:09 +0100

    add possbile move combitations and results

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = 'Nieznany ruch!';
 
 if (playerInput == '1'){
     playerMove = 'kamień';
@@ -29,3 +29,17 @@ if (playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (playerMove == 'Nieznany ruch!') {
+  printMessage ('Nieznany ruch!')  
+} else if (computerMove == playerMove) {
+  printMessage ('Remis!')  
+} else if (computerMove == 'kamień' && playerMove == 'papier') {
+  printMessage ('Ty wygrywasz!')
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage ('Ty wygrywasz!')
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage ('Ty wygrywasz!')
+} else {
+  printMessage ('Komputer wygrywa!')
+}
